08a5c558ccd2f468884b73959a6be2dcb0cdffb3
visualstudio <visualstudio@122-PC37>
2019-04-12 18:14:00 -0400

    code running commit

--- Gematria/Program.cs
+++ Gematria/Program.cs
@@ -8,9 +8,36 @@ namespace Gematria
 {
     class Program
     {
+        string[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
         static void Main(string[] args)
         {
             //writing the gemetria algorithm
+            Program p = new Program();
+            Console.WriteLine(p.Gemetria("universe"));
+
+        }
+        public  int Gemetria(string word)
+        {
+            int gematriaValue = 0;
+            for(int i=0;i<word.Length;i++)
+            {
+                gematriaValue += letterValue(word[i].ToString());
+            }
+            return gematriaValue;
+        }
+
+        private  int letterValue(string v)
+        {
+            int x = 0;
+            for(int a = 0; a < letters.Length; a++)
+            {
+                if (v.Equals(letters[a]))
+                {
+                    x = x + a;
+                }
+            }
+            return x;
         }
     }
+    
 }
